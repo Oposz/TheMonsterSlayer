aee0bd84ca5648a318ecd550c208e2af65ee4bc3
Przemek Gulik <przemek16391@wp.pl>
2021-04-28 14:40:41 +0200

    added Start new game functionality, some core futures, and battle log, end of project

--- app.js
+++ app.js
@@ -8,52 +8,70 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             round: 0,
-            winner:null,
+            winner: null,
+            logs:[]
         }
     },
     methods: {
         attackMonster() {
             this.round++;
             const attackValue = getRandomValue(5, 12);
+            const attackDisplay=`Player dealt ${attackValue}`
             this.monsterHealth -= attackValue;
+            this.logs.push(attackDisplay);
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
+            const attackDisplay=`Monster dealt ${attackValue}`
+            this.logs.push(attackDisplay);
             this.playerHealth -= attackValue;
         },
         specialAttack() {
             this.round++;
             const attackValue = getRandomValue(10, 25);
+            const attackDisplay=`Player dealt ${attackValue}`
             this.monsterHealth -= attackValue;
+            this.logs.push(attackDisplay);
             this.attackPlayer();
         },
         heal() {
             this.round++
             const healValue = getRandomValue(8, 20);
+            const healDisplay=`Player healed ${healValue}`
             if (this.playerHealth + healValue > 100) {
                 this.playerHealth = 100;
             } else {
                 this.playerHealth += healValue;
             }
+            this.logs.push(healDisplay);
             this.attackPlayer();
         },
-        surrender(){
-            this.round++;
-            this.playerHealth=0;
+        surrender() {
+            this.winner = 'monster';
+            const surrenderDisplay=`Player Surrendered`
+            this.logs.push(surrenderDisplay);
+            // this.playerHealth=0;
+        },
+        newGame() {
+            this.round = 0;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.logs=[];
         }
     },
     computed: {
         monsterBar() {
-            if (this.monsterHealth <= 0) {
-                return { display: "none" }
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
             } else {
                 return { width: this.monsterHealth + '%' }
             }
         },
         playerBar() {
-            if (this.playerHealth <= 0) {
-                return { display: "none" }
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
             } else {
                 return { width: this.playerHealth + '%' }
             }
@@ -66,19 +84,19 @@ const app = Vue.createApp({
         playerHealth(value) {
             if (value <= 0 && this.monsterHealth <= 0) {
                 // a draw
-                this.winner='draw'
+                this.winner = 'draw'
             } else if (value <= 0) {
                 // lost
-                this.winner='monster'
+                this.winner = 'monster'
             }
         },
         monsterHealth(value) {
             if (value <= 0 && this.playerHealth <= 0) {
                 // return a draw
-                this.winner='draw'
+                this.winner = 'draw'
             } else if (value <= 0) {
                 // win
-                this.winner='player'
+                this.winner = 'player'
             }
         }
     }
